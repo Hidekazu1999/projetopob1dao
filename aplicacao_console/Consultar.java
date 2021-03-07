dfc92ae9079b6738e3a18321dcb5f85d8132d15a
Hidekazu1999 <44981964+Hidekazu1999@users.noreply.github.com>
2021-03-07 18:54:43 -0300

    Update Consultar.java

--- aplicacao_console/Consultar.java
+++ aplicacao_console/Consultar.java
@@ -1,4 +1,27 @@
 package aplicacao_console;
 
+import fachada.Fachada;
+
 public class Consultar {
+    public Consultar(){
+        Fachada.inicializar();
+        try {
+            System.out.println(Fachada.consultarVideosPorAssunto(""));
+            System.out.println(Fachada.consultarVideoPorUsuario(""));
+            System.out.println(Fachada.consultarUsuariosPorVideo(""));
+            System.out.println(Fachada.localizarVisualizacao(0));
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Fachada.finalizar();
+        System.out.println("\nfim do programa");
+    }
+
+
+
+    //=================================================
+    public static void main(String[] args) {
+        new Consultar();
+    }
 }
