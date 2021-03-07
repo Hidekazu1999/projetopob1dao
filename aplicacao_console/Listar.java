a9eca051b023ab82bb2f8727b69e751e1bb1afdb
Hidekazu1999 <44981964+Hidekazu1999@users.noreply.github.com>
2021-03-07 19:40:39 -0300

    Update Listar.java

--- aplicacao_console/Listar.java
+++ aplicacao_console/Listar.java
@@ -1,4 +1,24 @@
 package aplicacao_console;
 
+import fachada.Fachada;
+
 public class Listar {
+    public Listar(){
+        Fachada.inicializar();
+        try {
+            System.out.println("Listando");
+            System.out.println(Fachada.listarVideos());
+            System.out.println(Fachada.listarVisualizacao());
+            System.out.println(Fachada.listarUsuario());
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Fachada.finalizar();
+    }
+
+    public static void main(String[] args) {
+        new Listar();
+    }
 }
