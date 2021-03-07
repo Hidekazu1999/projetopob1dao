ccf3270fb45814b049c5b51db56c18e312d63c8e
Hidekazu1999 <44981964+Hidekazu1999@users.noreply.github.com>
2021-03-07 14:38:20 -0300

    Update Atualizar.java

--- aplicacao_console/Atualizar.java
+++ aplicacao_console/Atualizar.java
@@ -1,4 +1,28 @@
 package aplicacao_console;
 
+import fachada.Fachada;
+
 public class Atualizar {
+      public Atualizar(){
+
+        Fachada.inicializar();
+
+        try {
+            System.out.println("Alterando");
+            Fachada.alterarTituloVideo("***", "Titulo alterado");
+            Fachada.alterarAssunto("****", "Assunto alterado");
+
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Fachada.finalizar();
+
+        System.out.println("fim do programa");
+    }
+
+    public static void main(String[] args) {
+        new Atualizar();
+    }
 }
