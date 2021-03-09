1a9efe15f71df2bf0306c51928f70808fa8957e7
Hidekazu1999 <44981964+Hidekazu1999@users.noreply.github.com>
2021-03-09 14:49:04 -0300

    Update Cadastrar.java

--- aplicacao_console/Cadastrar.java
+++ aplicacao_console/Cadastrar.java
@@ -1,26 +1,40 @@
 package aplicacao_console;
 
 import fachada.Fachada;
-import modelo.Video;
 
 public class Cadastrar {
 	public Cadastrar() {
 		try {
-			Fachada.inicializar();
-			Video v;
-			v=Fachada.cadastrarVideo("www.google.com", "Site", "Pesquisa");
-			//v=Fachada.cadastrarVideo("youtube.com/video","","Pesquisa");
-		}
-		catch(Exception e){
-			System.out.println(e.getMessage()); 
-		}
-		finally {
+
+			System.out.println("Cadastrando");
+
+			/*	Video	*/
+
+			Fachada.cadastrarVideo("youtube.com/resenha","videos engraçados","engraçado");
+			Fachada.cadastrarVideo("youtube.com/lol","Campeonato Cblol","lol");
+			Fachada.cadastrarVideo("youtube.com/fausto","Aprendendo Java","java");
+
+			/*	Usuarios	*/
+
+			Fachada.cadastrarUsuario("Hidekazu", "hidekazuo@ifpb.com","12345");
+			Fachada.cadastrarUsuario("Pablo", "pablo@ifpb.com", "123456");
+
+			/*	Visualizações 	*/
+
+			Fachada.cadastrarVisualizacao("youtube.com/resenha","pablo@ifpb.com", 12);
+			Fachada.cadastrarVisualizacao("youtube.com/lol","pablo@ifpb.com",22);
+			Fachada.cadastrarVisualizacao("youtube.com/fausto","hidekazuo@ifpb.com",102);
+
+
+		} catch (Exception e) 	{
+			System.out.println(e.getMessage());
+		} finally {
 			Fachada.finalizar();
 		}
-	}
-	public static void main(String[] args) {
-		
-		new Cadastrar();
+		System.out.println("fim do programa");
 	}
 
+	public static void main(String[] args) {
+		new Cadastrar();
+	}
 }
